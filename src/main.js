6596b0461ce0e3eae042e390dad401ffd803246c
zernellzeke <zernellzeke@gmail.com>
2026-07-13 11:41:55 +0200

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '../betting_app.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  React.createElement(React.StrictMode, null, React.createElement(App))
 );
